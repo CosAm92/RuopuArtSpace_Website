d82369ddcb6a016b90abd492c07d4947308cd32a
Amanda Dieuaide <amanda.dieuaide@eleve.isep.fr>
2021-12-22 19:47:58 +0100

    Search in artwork

--- routes/artworks.js
+++ routes/artworks.js
@@ -18,21 +18,68 @@ const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'] //We want the se
 
 
 //Show all artworks/the Art Gallery
-router.get('/', async (req, res) => {
-    let query = Artwork.find() //let != const (const reasigns the variable)
-    if (req.query.title != null && req.query.title != '') {
-        query = query.regex('title', new RegExp(req.query.title, 'i'))
+/*router.get('/', async (req, res) => {
+    /*let query = Artwork.find().sort({
+        createdAt: 'desc' //Top article = newest one
+    }).limit(parseInt(req.query.limit)) //let != const (const reasigns the variable)
+    
+
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const results = {}
+    if (endIndex < await Artwork.countDocuments().exec()) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        }
     }
 
-    try {
-        const artworks = await query.exec()
-        res.render('artworks/index', {
-            artworks: artworks,
-            searchOptions: req.query,
-        })
-    } catch {
-        res.redirect('/')
+    if (startIndex > 0) { //Check if we're at page 1
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        }
     }
+
+    res.paginationResults = results
+
+    try {
+        let query = Artwork.find().sort({
+            createdAt: 'desc' //Top article = newest one
+        }).limit(limit).skip(startIndex)
+
+        if (req.query.title != null && req.query.title != '') {
+            query = query.regex('title', new RegExp(req.query.title, 'i'))
+        }
+    
+        try {
+            const artworks = await query.exec()
+            //res.json(res.paginationResults)
+
+            res.render('artworks/index', {
+                artworks: artworks,
+                searchOptions: req.query,
+                //artworks: res.paginationResults.results,
+                next: res.paginationResults.next, 
+                previous: res.paginationResults.previous,
+                title: req.query.title 
+            })
+        } catch {
+            res.redirect('/')
+        }
+
+        //next()
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+})*/
+
+
+router.get('/', paginationResults(Artwork), async (req, res) => {
+    res.render('artworks/index', { artworks: res.paginationResults.results, next: res.paginationResults.next, previous: res.paginationResults.previous, searchOptions: req.query })
 })
 
 //New artwork route
@@ -50,8 +97,8 @@ router.post('/', async (req, res) => { //The library multer does the work of put
         createdAt: new Date(req.body.createdAt),
         //image: fileName,
         content: req.body.content,
-        theme : req.body.theme,
-        tags : JSON.stringify(req.body.tags).replace(/]|[[]/g, '').replace(/\"/g, "").split(",") 
+        theme: req.body.theme,
+        tags: JSON.stringify(req.body.tags).replace(/]|[[]/g, '').replace(/\"/g, "").split(",")
     })
 
     saveImage(artwork, req.body.image)
@@ -100,7 +147,7 @@ router.put('/:id', async (req, res) => {
         artwork.createdAt = new Date(req.body.createdAt)
         artwork.content = req.body.content
         artwork.theme = req.body.theme
-        artwork.tags = JSON.stringify(req.body.tags).replace(/]|[[]/g, '').replace(/\"/g, "").split(",") 
+        artwork.tags = JSON.stringify(req.body.tags).replace(/]|[[]/g, '').replace(/\"/g, "").split(",")
         if (req.body.image != null && req.body.image !== '') {
             saveImage(artwork, req.body.image)
         } //The default is null, we don't want to delete the cover
@@ -180,6 +227,61 @@ function saveImage(artwork, imageEncode) {
     if (image != null && imageMimeTypes.includes(image.type)) {
         artwork.image = new Buffer.from(image.data, 'base64')
         artwork.imageType = image.type
+    }
+}
+
+///PAGINATION
+function paginationResults(model) {
+    return async (req, res, next) => {
+
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+        var title = req.query.title
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+        //title: { $regex: /^title/i}
+        let test = await model.find({ title: { $regex: title }}).countDocuments()
+        if (title == '' || title == null) {
+            test = await model.find().countDocuments()
+        }
+
+        const results = {}
+        if (endIndex < test) {
+            results.next = {
+                page: page + 1,
+                limit: limit,
+                title: title
+            }
+        }
+
+        if (startIndex > 0) { //Check if we're at page 1
+            results.previous = {
+                page: page - 1,
+                limit: limit,
+                title: title
+            }
+        }
+
+        try {
+            /*results.results = await model.find().sort({
+                createdAt: 'desc' //Top article = newest one
+            }).limit(limit).skip(startIndex).exec()*/
+
+            let query = model.find({ title: { $regex: title }}).sort({
+                createdAt: 'desc' //Top article = newest one
+            }).limit(limit).skip(startIndex)
+
+            /*if (title != null && title != '') {
+                query = query.regex('title', new RegExp(title, 'i'))
+            }*/
+
+            results.results = await query.exec()
+
+            res.paginationResults = results
+            next()
+        } catch (e) {
+            res.status(500).json({ message: e.message })
+        }
     }
 }
 module.exports = router
